872efedbf83d852b704e953e16de4ced47917ab1
tatsuroaoki <tatsuro.aoki@gmail.com>
2018-07-12 19:37:04 -0700

    window tab shows klick count and added font change when hitting 10 and 20 klicks

--- script.js
+++ script.js
@@ -7,34 +7,40 @@ var clicks = 0;
 // clicker handler function. Assign handlers to page events.
 window.onload = function(){
 
-  document.querySelector("img#pic").addEventListener("click", clickCounter)
+	document.querySelector("img#pic").addEventListener("click", clickCounter)
 
 }
 
-  // track total clicks - increment the counter variable
+// track total clicks - increment the counter variable
 function clickCounter(event) {
-    clicks++;
+	clicks++;
 
-    outputClicks();
+	outputClicks();
 }
 
-  // output click count to container in DOM
+// output click count to container in DOM
 
 
-  function outputClicks(){
-    var output = document.getElementById("result");
-    /*
+function outputClicks(){
+	var output = document.getElementById("result");
+	var output2 = document.getElementById("title").innerHTML = clicks;
+	/*
       Clear the existing contents of the "list" element. Then, for each object in courseList,
       create an li element that holds the course's name and grade, and append
       it to the "list" ul element.
     */
-      output.innerHTML = "";
-      console.log(output);
+	output.innerHTML = "";
+	console.log(output);
 
-      if (clicks === 1) {
-        output.innerHTML = "Koala has been klicked " + clicks + " time.";
-      } else {
-        output.innerHTML = "Koala has been klicked " + clicks + " times.";
-      }
+	if (clicks === 1) {
+		output.innerHTML = "Koala has been klicked " + clicks + " time.";
+	} else {
+		output.innerHTML = "Koala has been klicked " + clicks + " times.";
+	}
 
-  }
+	if(clicks === 10 || clicks == 20) {
+		output.setAttribute("class","color");
+	} else {
+		output.removeAttribute("class","color");
+	}
+}
